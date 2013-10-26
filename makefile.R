f628b2d3b8051e55a6471ee64f346b60f71b4cfe
Amanda Yuen <ayuen709@gmail.com>
2013-10-25 18:30:22 -0700

    Master R Script for Homework #6.

---
+++ makefile.R
@@ -0,0 +1,7 @@
+============================
+Homework #6 Master R Script
+============================
+  
+## Run R Scripts 1 and 2
+source("01_reordersortorigdata.R")
+source("02_linregressnewdata.R")
